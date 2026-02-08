df7611e653b96cb049874de237a6e8852277ad98
Denis Darkovich <darkovichdenis@gmail.com>
2026-02-08 19:35:05 +0300

    Revert "add a data model"
    This reverts commit d353d5bbb12b8f20e15adcb873562615cb6af39e.

--- src/main/java/org/example/model/MembershipType.java
+++
@@ -1,7 +0,0 @@
-package org.example.model;
-
-public enum MembershipType {
-    SINGLE,
-    MONTHLY,
-    YEARLY
-}
--- src/main/java/org/example/model/Status.java
+++
@@ -1,7 +0,0 @@
-package org.example.model;
-
-public enum Status {
-    SCHEDULED,
-    COMPLETED,
-    CANCELLED
-}
