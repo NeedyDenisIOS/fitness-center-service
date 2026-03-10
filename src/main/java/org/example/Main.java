d4b541df3d302805889579d2b9717b944862fe86
Denis Darkovich <darkovichdenis@gmail.com>
2026-03-10 17:08:11 +0300

    Adding main menu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,64 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.controller.MainMenu;
+import org.example.repository.MemberRepository;
+import org.example.repository.WorkoutRepository;
+import org.example.service.MemberServiceImpl;
+import org.example.service.WorkoutServiceImpl;
+import org.example.util.FileDataService;
+import org.example.validation.MemberValidator;
+import org.example.validation.WorkoutValidator;
+import org.example.validation.exception.DataNotFoundException;
+import org.example.validation.exception.MemberValidationException;
+import org.example.validation.exception.WorkoutValidationException;
+
 public class Main {
-    static void main() {
-        System.out.println("Hello");
+
+    public static void main(String[] args) throws DataNotFoundException, WorkoutValidationException, MemberValidationException {
+
+        MemberValidator memberValidator = new MemberValidator();
+        WorkoutValidator workoutValidator = new WorkoutValidator();
+
+        MemberRepository memberRepository = MemberRepository.getInstance();
+        WorkoutRepository workoutRepository = WorkoutRepository.getInstance();
+
+        WorkoutServiceImpl workoutService = new WorkoutServiceImpl(workoutValidator);
+        MemberServiceImpl memberService = new MemberServiceImpl(memberValidator, workoutValidator);
+
+
+        FileDataService fileDataService = new FileDataService("D:\\Java\\fitness-center-service\\src\\main\\java\\org\\example", memberRepository, workoutRepository);
+
+        MainMenu mainMenu = new MainMenu(memberService, workoutService);
+
+        fileDataService.loadData();
+
+        boolean running = true;
+
+        while (running) {
+            try {
+                mainMenu.printMainMenu();
+
+                int choice = mainMenu.getUserChoice();
+
+                if (choice == 0) {
+                    running = false;
+                    System.out.println("\n Exiting...");
+                } else if (choice > 0) {
+                    mainMenu.handleChoice(choice);
+                }
+
+            } catch (DataNotFoundException e) {
+                System.out.println("Not found: " + e.getMessage());
+            } catch (WorkoutValidationException e) {
+                System.out.println("Workout error: " + e.getMessage());
+            } catch (MemberValidationException e) {
+                System.out.println("Member error: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Unexpected error: " + e.getMessage());
+            }
+        }
+        fileDataService.saveData();
     }
+
+
 }
